c7573f015ea1349e19c5a76252c9de21d265d2d3
philiphaex <philip_haex@hotmail.com>
2017-04-08 19:33:54 +0200

    preparing views before customizing register

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,18 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('firstname');
+            $table->string('lastname');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('street');
+            $table->string('street_number');
+            $table->string('street_bus_number');
+            $table->string('zipcode');
+            $table->integer('phone');
+            $table->integer('mobile');
+            $table->boolean('confirmed')->default(0);
+            $table->string('confirmation_code')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
