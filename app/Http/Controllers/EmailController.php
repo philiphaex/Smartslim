c0408cce703f1006e82d8cbf1dcf3656edb6d72b
philiphaex <philip_haex@hotmail.com>
2017-04-09 02:17:08 +0200

    email template via bee.io

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Http\Request;
 
 class EmailController extends Controller
 {
 	public function send(Request $request)
 	{
-
+		$data=[];
+		Mail::send('emails.verify', $data, function($message) {
+			$message->to('philip_haex@hotmail.com','philip')
+				->subject('Verify your email address');
+		});
     }
 }
