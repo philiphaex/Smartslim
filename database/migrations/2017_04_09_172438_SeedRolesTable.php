dfd14595a5b52a68e490f88497ab174361a99d95
philiphaex <philip_haex@hotmail.com>
2017-04-09 20:58:36 +0200

    SeedRolesTable toegevoegd

---
+++ database/migrations/2017_04_09_172438_SeedRolesTable.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class SeedRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::table('roles')->insert(
+        array(
+            array('name' => 'admin',
+                  'display_name' =>'Start',
+                  'description' =>'Gratis pakket, cliëntbeheer, max 10 cliënten per maand'),
+            array('name' => 'level1',
+                'display_name' =>'Basis',
+                'description' =>'Basis pakket, agenda, max 30 cliënten per maand' ),
+            array('name' => 'level2',
+                'display_name' =>'Professioneel',
+                'description' =>'Professioneel pakket, cliëntenlogboek, max 60 cliënten per maand' ),
+        ));
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
