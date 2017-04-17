582d058ea50ba82f553f9e92973534c8c9c79501
philiphaex <philip_haex@hotmail.com>
2017-04-17 23:04:11 +0200

    Toevoeging extra tabellen

---
+++ database/migrations/2017_04_17_214527_create_prices_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Prices extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prices', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('price');
+            $table->integer('role_id');
+            $table->boolean('active');
+            $table->string('description');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prices');
+
+    }
+}
