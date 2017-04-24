5cfc07e63d6027a501b7bd608a51fffff8a6a827
philiphaex <philip_haex@hotmail.com>
2017-04-24 21:59:33 +0200

    start online payment

--- database/migrations/2017_04_17_214527_create_prices_table.php
+++ database/migrations/2017_04_17_214527_create_prices_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Prices extends Migration
+class CreatePricesTable extends Migration
 {
     /**
      * Run the migrations.
