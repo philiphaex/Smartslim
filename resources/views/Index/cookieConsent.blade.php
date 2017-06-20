f613355c499e4b5348ea49f96b89606a6c0c21eb
philiphaex <philip_haex@hotmail.com>
2017-06-20 22:45:18 +0200

    update frontend

--- resources/views/Index/cookieConsent.blade.php
+++ resources/views/Index/cookieConsent.blade.php
@@ -1,15 +1,3 @@
-{{--
-<div class="panel panel-default" id="cookieConsent">
-    <div class="panel-body">
-        <div class="container">
-        Deze website maakt gebruik van cookies om de surfervaring te verbeteren. Door gebruik te maken van deze website staat u het gebruik van cookies toe.
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>
-        </div>
-    </div>
-</div>
---}}
 @if(Cookie::get('smartslim') != 'visited')
 <div class="alert" id="cookieConsent">
         <div class="container">
--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -16,6 +16,7 @@
     <link href="{{ asset('css/main.css') }}" rel="stylesheet">
     {{--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/paper/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">--}}
     {{--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">--}}
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 
 
     <!-- Scripts -->
