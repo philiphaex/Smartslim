6d61b63aafee492674be0011851d5ebbbb900053
philiphaex <philip_haex@hotmail.com>
2017-06-22 22:32:34 +0200

    update app config

--- config/app.php
+++ config/app.php
@@ -51,7 +51,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost/smartslim/public'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     /*
     |--------------------------------------------------------------------------
